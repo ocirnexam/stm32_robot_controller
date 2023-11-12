28a1fad0ef12f556790732e32385b89a421ccc92
ocirnexam <kontakt.maxmueller@outlook.com>
2023-11-12 11:35:06 +0100

    Added test for moving servo with potentiometer (Read works, write not)

--- src/main.c
+++ src/main.c
@@ -27,17 +27,18 @@ int main(void)
   USART_Init_PC();
   
   // Alt function PB3 is connected to TIM2_CH2
+  IOPin pot_ax0 = {GPIOA, PIN0, GPIO_ANALOG_MODE, ADC1_IN0};
   IOPin servo_ax0 = {GPIOB, PIN4, GPIO_ALT_FUNCION, GPIO_ALTF_TIM3_TIM5};
   Servo servo = {servo_ax0, TIM3, 1};
+  GPIO_Config(pot_ax0);
   Servo_Init(servo);
-  // GPIO_Config(servo_ax0);
-  volatile int i = 0;
   while (1)
   {
-    Servo_Write(servo, i++%5);
-    // sprintf((char *)buffer, "PB4 Output: %ld\r\n", GPIOB->ODR);
-    // send_string_pc((char *) buffer);
-    msleep(500);
+    volatile float pot_value = (float) GPIO_ReadAnalogPin(pot_ax0);
+    volatile uint16_t servo_value = (uint16_t) (( pot_value / 250) * 2000) + 500;
+    Servo_Write(servo, servo_value);
+    sprintf((char *)buffer, "Servo Value: %d\r\n", servo.timer->CCR1);
+    send_string_pc((char *) buffer);
   }
   return 0;
 }
