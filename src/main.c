6c9188401cfa2a5a7c0883e59d8642e37c84bd62
Max <maximilian.en.mueller@swisscows.email>
2025-04-12 22:57:16 +0200

    Updated stm32f4_mcal and added test in main to check full servo cycle

--- src/main.c
+++ src/main.c
@@ -25,20 +25,27 @@ int main(void)
     return COUNTDOWN_EXCEEDED_ERROR;
   }
   USART_Init_PC();
-  
+
   // Alt function PB3 is connected to TIM2_CH2
-  IOPin pot_ax0 = {GPIOA, PIN0, GPIO_ANALOG_MODE, ADC1_IN0};
+  // IOPin pot_ax0 = {GPIOA, PIN0, GPIO_ANALOG_MODE, ADC1_IN0};
   IOPin servo_ax0 = {GPIOB, PIN4, GPIO_ALT_FUNCION, GPIO_ALTF_TIM3_TIM5};
   Servo servo = {servo_ax0, TIM3, 1};
-  GPIO_Config(pot_ax0);
-  Servo_Init(servo);
+  // GPIO_Config(pot_ax0);
+  Servo_Init(&servo);
+  uint8_t i = 0;
+  int8_t operand = 1;
   while (1)
   {
-    volatile float pot_value = (float) GPIO_ReadAnalogPin(pot_ax0);
-    volatile uint16_t servo_value = (uint16_t) (( pot_value / 250) * 2000) + 500;
-    Servo_Write(servo, servo_value);
-    sprintf((char *)buffer, "Servo Value: %d\r\n", servo.timer->CCR1);
-    send_string_pc((char *) buffer);
+    if (i == 180) {
+      operand = -1;
+    } else if (i == 0) {
+      operand = 1;
+    }
+    Servo_Write(&servo, i);
+    sprintf(buffer, "PWM duty cycle steps: %d\r\n", servo.timer->CCR1);
+    send_string_pc(buffer);
+    i += operand;
+    msleep(1);
   }
   return 0;
 }
